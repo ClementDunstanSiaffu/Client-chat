8dcba409457d5906ae27008cac160348350a90e9
ClementDustanSiaffu <clementsiaffu95@gmail.com>
2020-06-19 17:29:34 +0300

    17 th change

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ function Facial(){
          
         <div style={{overFlow:"hidden",height:625}} className ="back-facial">
        <video preload="metadata" playsinline autoPlay loop muted  webkit-playsinline
-       style={{paddingRight:0,objectFit: 'cover',opacity:0.8,width:"100%",height:625}}>
+       style={{paddingRight:0,objectFit: 'cover',opacity:0.8,width:"100%",height:625,marginBottom:-60}}>
             <source src={logo} type = "video/mp4" />
           
        </video>
